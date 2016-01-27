699372af76e80fe848e8a27385c0850022b20610
jcoombes <coombes.jd@gmail.com>
2016-01-27 18:02:18 +0000

    Create properties.py

---
+++ properties.py
@@ -0,0 +1,13 @@
+'''
+A module filled with particle properties.
+
+Useful to ensure the same value is used for every constant.
+'''
+pion_mass = 139.6 #MeV
+pion_lifetime = 2.6E−8 #s
+
+muon_mass = 105.7 #MeV
+muon_lifetime = 2.2E−6 #s
+
+electron_mass = 0.5 #MeV
+#electron lifetime is so long we can assume it lives forever.
