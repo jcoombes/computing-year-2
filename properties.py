d98380149443eca9f1a2d18a68b87021c9a437b3
jcoombes <coombes.jd@gmail.com>
2016-01-30 20:49:21 +0000

    Update properties.py

--- properties.py
+++ properties.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
 '''
 A module filled with particle properties.
 
 Useful to ensure the same value is used for every constant.
 '''
 pion_mass = 139.6 #MeV
-pion_lifetime = 2.6E−8 #s
+pion_lifetime = 2.6e-8 #s
 
 muon_mass = 105.7 #MeV
-muon_lifetime = 2.2E−6 #s
+muon_lifetime = 2.2e-6 #s
 
 electron_mass = 0.5 #MeV
 #electron lifetime is so long we can assume it lives forever.
+
+# 1eV^-1 of length = 1.97327e-7 m
+# 1eV of mass = 1.782662e-36 kilograms
+# 1eV-1 of time = 6.582119e-16 seconds
