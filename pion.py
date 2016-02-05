1065bcafab25f7cf6d886f9a82be70f0c866beb4
jcoombes <coombes.jd@gmail.com>
2016-02-05 03:12:00 +0000

    Made pions decay. Changed movement.

--- pion.py
+++ pion.py
@@ -15,22 +15,19 @@ class Particle(object):
     
     Inputs:
         e - energy. float. measured in MeV.
+        born - initial location. expect len(born)==3.
+        k - direction of travel.
         m - mass. float. measured in MeV
         tau - lifetime. measured in s.
-        k - direction of travel. defaults to z-axis.
-        born - initial location. expect len(born)==3.
-        died - final location. expect len(died)==3.
-
     """
     
-    def __init__(self, e = 500, m=0, tau = 1, k = [0,0,1], born = [0,0,0], died = [0,0,0]):
+    def __init__(self, e, born, k, m, tau):
         #Make sure to encapsulate data at some point.
         self.e = e #MeV
         self.m = m #MeV
         self.tau = tau
         self.k = np.array(k, dtype='float64')
         self.born = np.array(born, dtype = 'float64')
-        self.died = np.array(died, dtype = 'float64')
         self.life = tau
         
         self.g = self.e/self.m
@@ -41,65 +38,130 @@ class Particle(object):
         self.pvec = self.p * self.knorm #momentum vector.
         
         self.emom = fv.FourVector(self.e, self.pvec) #energy momentum 4-vector.
-        
-    def move(self, lab = True):
+        #Muon position 4-vector currently uses lifetime in own frame, position in lab frame
+        #Muon energy 4-vector currently uses energy in pion frame. Momentum in momentum/ab frame.
+    
+    def move(self):
         """
         propagates particle for length of life in lab frame.
         
         This method will only work for derived classes (with defined self.life)
-        lab - Bool. Are we already in lab frame? if False, we need to convert.
-        
         Output distances in light-seconds as c=1.
+        Use pion.props.ls2m() for an output in metres.  
         """
-        if lab:
-            self.died = self.born + self.knorm * self.life * self.b
-        else:
-            self.died = self.born + self.knorm * self.life * self.b * self.g 
-            
+        rest_position = fv.FourVector(self.life, self.born)
+        lab_position = rest_position.super_boost(-self.b, self.knorm)
+        #This line currently converts muon frame to some other frame. Not lab!!!
+        self.died = lab_position.r
         return self.died
-
+    
+    def decay_direction(self):
+        """
+        Randomly generates a uniform vector on the unit sphere.
+        """
+        u = np.random.sample()
+        v = np.random.sample()
+        phi = 2*np.pi*u
+        theta = np.arccos(2*v-1) #mathworld.wolfram.com/SpherePointPicking.html
+        
+        x = np.sin(theta) * np.cos(phi)
+        y = np.sin(theta) * np.sin(phi)
+        z = np.cos(theta)
+        return np.array([x,y,z])
+    """
+    def out_of_bounds(self):
+        if
+    """
 class Pion(Particle):
     """
     It's a pion. All our particles start off as these.
     """
-    pion_lives = np.random.exponential(props.pion_lifetime,1E8)
+    pion_lives = np.random.exponential(props.pion_lifetime,2E7)
     seed = 0 # so we can get back the same exp-distributed rand numbers.
     #Code is faster if we generate lots at once then look it up.
     
-    rollcall = []
+    instances = []
     
-    def __init__(self, e = 500, m = props.pion_mass , tau = props.pion_lifetime, branch = 0.5):
-        super(Pion, self).__init__(e, m, tau)
+    def __init__(self, e, born = [0,0,0], k = [0,0,1], m = props.pion_mass , tau = props.pion_lifetime, branch = 0.0001):
+        super(Pion, self).__init__(e, born, k, m, tau)
         self.branch = branch #Branching Ratio. REALLY IMPORTANT.
         self.life = self.pion_lives[self.seed]
         Pion.seed += 1
-        Pion.rollcall.append(self)
+        Pion.instances.append(self)
     
-    
+    def move(self):
+        """
+        Special faster pion method to move faster than the general lorenz boost.
+        propagates particle for length of life in lab frame.
+        
+        Output distances in light-seconds as c=1.
+        See ls2m function in properties file for output in metres.
+        """
+        self.died = self.knorm * self.life * self.b * self.g  
+        return self.died
+       
+    def decay(self):
+        """
+            Deletes particle.
+            Makes new particle.
+            
+            Note if self.died hasn't been set by move() the particle decays in place.
+        """
+        if np.random.sample() > self.branch:
+            
+            energy = (props.pion_mass*props.pion_mass
+                    +props.muon_mass*props.muon_mass)/(2*props.pion_mass)
+                    #Assumes zero neutrino mass. 
+            return Muon(energy, self.died, self.decay_direction())
+            
+        else:leccy
+            energy = (props.pion_mass*props.pion_mass
+                    +props.electron_mass*props.electron_mass)/(2*props.pion_mass)
+                    #Assumes zero neutrino mass. 
+            return Electron(energy, self.died, self.decay_direction(),parent = 'Pion')
     """
-    def decay():
-        raise NotImplementedError
-    
-    def detect():
+    def detect(self):
         raise NotImplementedError
     """
 
-"""
+
 class Muon(Particle):
-    raise NotImplementedError
-    Muon.muon_lives = np.random.exponential(props.muon_lifetime, 1E7)
-
-class Electron(Particle):
-    raise NotImplementedError
-"""
-
-def ls2m(lightseconds):
     """
-    Convert light seconds to natural units.
-    Only use this to make output more human friendly.
-    Not built to do any calculations in metres.
-    
+    It's a muon. Pions decay into this.
     Inputs:
-        lightseconds - float.
+        e - total energy.
+        born - initial position. 3-position.
+        k - initial direction in created frame.
     """
-    return lightseconds*299792458
+    muon_lives = np.random.exponential(props.muon_lifetime, 2e7)
+    seed = 0 # so we can get back the same exp-distributed rand numbers.
+    #Code is faster if we generate lots at once then look it up.
+    
+    instances = []
+    
+    def __init__(self, e, born, k, m = props.muon_mass, tau = props.muon_lifetime):
+        super(Muon, self).__init__(e, born, k, m, tau)
+        self.life = self.muon_lives[self.seed]
+        Muon.seed += 1
+        Muon.instances.append(self)
+    
+    def decay(self):
+        """
+        Unlike pion decays, muons can only decay into electrons.
+        """
+        return Electron(props.michel(), self.died, self.decay_direction(), parent = 'Muon')
+        
+    
+class Electron(Particle):
+    """
+    It's an electron. Electrons have an extra parameter showing their parent.
+    """
+    instances = []
+    
+    def __init__(self, e, born, k, m = props.electron_mass, parent = 'Pion'):
+        tau = 1
+        super(Electron, self).__init__(e, born, k, m, tau)
+        self.parent = parent
+        #Easier to have both pion and muon inherit from particle,
+        #and delete tau in electron, than implement for both pion and muon.
+        Electron.instances.append(self)
