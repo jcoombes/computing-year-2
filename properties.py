3cd1f1dca8182430dfab5bbce78d2480532c85b1
jcoombes <coombes.jd@gmail.com>
2016-02-06 23:51:04 +0000

    added chamber dimension in light-seconds.

--- properties.py
+++ properties.py
@@ -17,6 +17,8 @@ electron_mass = 0.5 #MeV
 #electron lifetime is so long we can assume it lives forever.
 #also neutrino mass is 0.320eV. We can assume they're massless.
 
+chamber_z = 3.335640952e-7 # light seconds
+chamber_r = 8.339102380e-9 #light seconds
 # 1eV^-1 of length = 1.97327e-7 m
 # 1eV of mass = 1.782662e-36 kilograms
 # 1eV-1 of time = 6.582119e-16 seconds
