846a93b5bbdf85fc101c2bc6d32e2ea8dc9c4151
jcoombes <coombes.jd@gmail.com>
2016-02-07 20:53:20 +0000

    Update pion.py

--- pion.py
+++ pion.py
@@ -22,7 +22,11 @@ class Particle(object):
     """
     
     def __init__(self, e, born, k, m, tau):
-        #Make sure to encapsulate data at some point.
+        """
+        Make sure e is greater than rest mass of particle.
+        """
+        if e < m:
+            raise ValueError('particle energy is less than the rest mass energy.')
         self.e = e #MeV Lab frame. #2:pf
         self.m = m #MeV
         self.tau = tau #Particle frame. #2:pf
@@ -39,8 +43,10 @@ class Particle(object):
         
         self.emom = fv.FourVector(self.e, self.pvec) #energy momentum 4-vector.
         #Muon position 4-vector currently uses lifetime in own frame, position in lab frame
-        #Muon energy 4-vector currently uses energy in pion frame. Momentum in momentum/lab frame.
-    
+        #Muon energy 4-vector currently uses energy in pion frame. Momentum in momentum/lab frame.    
+        
+        self.walls = None
+        
     def move(self):
         """
         propagates particle for length of life in lab frame.
@@ -53,7 +59,8 @@ class Particle(object):
         self.lab_pvec = self.lab_emom.r
         self.lab_e = self.lab_emom.ct
         self.lab_u = self.lab_pvec/self.lab_e
-        self.died = self.born + self.lab_u * Pion.instances[-1].g * self.life
+        self.lab_life = Pion.instances[-1].g * self.life
+        self.died = self.born + self.lab_u * self.lab_life
         return self.died
     
     def decay_direction(self):
@@ -75,17 +82,43 @@ class Particle(object):
         Method to determine location particle ultimately intersects detector walls.
         Use after particle.move()
         """
-        t1 = 0-self.born[2]/self.lab_u # z = 0
-        t2 = 100-self.born[2]/self.lab_u # z = 100
+        t1 = (0-self.born[2])/self.lab_u[2] # z = 0
+        t2 = (props.chamber_z-self.born[2])/self.lab_u[2] # z = 100
         
         quant = (self.born[0]*self.lab_u[0] + self.born[1]*self.lab_u[1])
         
         t3 = (-quant+np.sqrt(quant*quant \
-        - (self.lab_u.dot(self.lab_u)*(self.born[:2].dot(self.born[:2])-6.25))))\
+        - (self.lab_u.dot(self.lab_u)*\
+        (self.born[:2].dot(self.born[:2])-(props.chamber_r*props.chamber_r)))))\
         /self.lab_u.dot(self.lab_u)
         
         first_intersection_time = min([t for t in [t1, t2, t3] if t>0])
-        return self.born + first_intersection_time*self.lab_u        
+        hits = self.born + first_intersection_time*self.lab_u
+        
+        if first_intersection_time <= self.lab_life: #hits wall before it decays
+            self.walls = hits
+            return hits
+        else:
+            return None
+         
+    def inside(self):
+        """
+        Return true if a particle is inside chamber.
+        Use after particle.move().
+        """
+        if hasattr(self, 'walls') and self.walls is not None:
+            x = self.walls
+        elif hasattr(self, 'died') and self.died is not None:
+            x = self.died
+        else:
+            x = self.born
+        
+        if x[0]*x[0] + x[1] * x[1] <= props.chamber_r*props.chamber_r:
+            return True
+        elif 100 >= x[2] >= 0:
+            return True
+        else:
+            return False
         
     
 class Pion(Particle):
@@ -98,7 +131,7 @@ class Pion(Particle):
     
     instances = []
     
-    def __init__(self, e, born = [0,0,0], k = [0,0,1], m = props.pion_mass , tau = props.pion_lifetime, branch = 0.1):
+    def __init__(self, e, born = [0,0,0], k = [0,0,1], m = props.pion_mass , tau = props.pion_lifetime, branch = 0.0001):
         super(Pion, self).__init__(e, born, k, m, tau)
         self.branch = branch #Branching Ratio. REALLY IMPORTANT.
         self.life = self.pion_lives[self.seed]
@@ -135,16 +168,40 @@ class Pion(Particle):
                     +props.electron_mass*props.electron_mass)/(2*props.pion_mass)
                     #Assumes zero neutrino mass. 
             return Electron(energy, self.died, self.decay_direction(),parent = 'Pion')
+    
+    def hits_walls(self):
+        """
+        Specific pion method to detect if pion hits the back wall of the chamber.
+        Returns None if False, Returns co-ordinates if true.
+        """
+        if self.died[2] >= props.chamber_z:
+            self.walls = np.array([0,0,props.chamber_z], dtype = 'float64')
+            return self.walls
     """
     def detect(self):
         raise NotImplementedError
     """
     
-    """
-    def __repr__(self):
-        #Note that I need to use energy, self.born, self.self.hits, detected?
-        raise NotImplementedError
-    """
+    
+    def __str__(self):
+        '''Returns a string representation of particle.
+        This output should be used to debug.
+        Do not use the output of this to create new pions.'''
+        #Note that I need to use energy, self.born, self.hits, detected?
+        if hasattr(self, 'walls'):
+            if hasattr(self, 'detected'):
+                ans = 'Pion({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, self.walls, self.detected)
+            else:
+                ans = 'Pion({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, self.walls, 'None')
+        elif hasattr(self, 'detected'):
+            ans = 'Pion({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, 'None ',self.detected)
+        else:
+            ans = 'Pion({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, 'None ','None')
+        return ans
 
 
 class Muon(Particle):
@@ -172,7 +229,29 @@ class Muon(Particle):
         Unlike pion decays, muons can only decay into electrons.
         """
         return Electron(props.michel(), self.died, self.decay_direction(), parent = 'Muon')
+   
+    def __str__(self):
+        '''
+        Returns a string representation of the particle.
+        This output should be used to debug.
+        Do not use this output to make new muons.
+        '''
+        #Note that I need to use energy, self.born, self.hits, detected?
+        if hasattr(self, 'walls'):
+            if hasattr(self, 'detected'):
+                ans = 'Muon({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, self.walls, self.detected)
+            else:
+                ans = 'Muon({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, self.walls, 'None')
+        elif hasattr(self, 'detected'):
+            ans = 'Muon({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, 'None ',self.detected)
+        else:
+            ans = 'Muon({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, 'None ','None')
         
+        return ans 
     
 class Electron(Particle):
     """
@@ -187,3 +266,27 @@ class Electron(Particle):
         #Easier to have both pion and muon inherit from particle,
         #and delete tau in electron, than implement for both pion and muon.
         Electron.instances.append(self)
+        
+    
+    def __str__(self):
+        '''
+        Returns a string representation of the particle.
+        This output should be used to debug.
+        Do not use this output to make new electrons.
+        '''
+        #Note that I need to use energy, self.born, self.hits, detected?
+        if hasattr(self, 'walls'):
+            if hasattr(self, 'detected'):
+                ans = 'Electron({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, self.walls, self.detected)
+            else:
+                ans = 'Electron({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, self.walls, 'None')
+        elif hasattr(self, 'detected'):
+            ans = 'Electron({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, 'None ',self.detected)
+        else:
+            ans = 'Electron({},died = {}, walls = {}, detected = {})'\
+                .format(self.e, self.died, 'None ','None')
+        
+        return ans 
